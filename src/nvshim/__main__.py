c674168715f1be39f3e047cf6327f30e49c618d6
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-09-07 08:17:38 -0400

    wip: resolve aliases to semver

--- src/nvshim/__main__.py
+++ src/nvshim/__main__.py
@@ -1,10 +1,20 @@
-import os
+import functools
 import json
+import os
+from typing import Dict
+
 from colored import fg, bg, attr
 import semver
 
 COLOR_ERROR = fg("red")
 COLOR_RESET = attr("reset")
+
+AliasMapping = Dict[str, str]
+
+
+class HashableDict(dict):
+    def __hash__(self):
+        return hash(frozenset(self.items()))
 
 
 def print_error(text: str):
@@ -38,15 +48,54 @@ def get_files(path: str) -> [str]:
         yield path
 
 
-def get_nvm_aliases():
-    nvm_aliases_dir = os.path.join(get_nvm_dir(), "alias")
-    aliases_to_version = {}
-    for file_path in get_files(nvm_aliases_dir):
-        rel_path = os.path.relpath(file_path, nvm_aliases_dir)
+def get_nvm_alias_dir(nvm_dir):
+    return os.path.join(nvm_dir, "alias")
+
+
+def get_nvm_aliases(nvm_alias_dir: str) -> AliasMapping:
+    aliases_to_version = HashableDict()
+    for file_path in get_files(nvm_alias_dir):
+        rel_path = os.path.relpath(file_path, nvm_alias_dir)
         with open(file_path) as f:
             aliases_to_version[rel_path] = f.readline().strip()
 
     return aliases_to_version
+
+
+def parse_version(version: str) -> semver.VersionInfo:
+    try:
+        if version.startswith("v"):
+            version = version[1:]
+        return semver.parse_version_info(version)
+    except ValueError:
+        pass
+
+
+@functools.lru_cache(maxsize=None)
+def resolve_alias(
+    name: str, alias_mapping: AliasMapping
+) -> (str, [str], semver.VersionInfo):
+    seen_in_order = []
+    seen = set()
+    while name in alias_mapping and name not in seen:
+        name = alias_mapping.get(name)
+        seen_in_order.append(name)
+        seen.add(name)
+
+    return name, seen_in_order, parse_version(name)
+
+
+def resolve_nvm_aliases(nvm_aliases: AliasMapping):
+    resolved_aliases = {}
+    for alias, version in nvm_aliases.items():
+        version_info = parse_version(version)
+        if not version_info:
+            _, __, version_info = resolve_alias(version, nvm_aliases)
+
+        if version_info:
+            resolved_aliases[alias] = str(version_info)
+
+    return resolved_aliases
 
 
 # get mapping of aliases to node versions
@@ -54,4 +103,7 @@ def get_nvm_aliases():
 # get nvmrc or use default
 # return path to node or npm
 if __name__ == "__main__":
-    print(get_nvm_aliases())
+    nvm_dir = get_nvm_dir()
+    nvm_alias_dir = get_nvm_alias_dir(nvm_dir)
+    nvm_aliases = get_nvm_aliases(nvm_alias_dir)
+    print(resolve_nvm_aliases(nvm_aliases))
