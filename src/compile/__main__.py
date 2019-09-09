4975ab5b280f39eaea225870cc22e09a3c3fd241
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-09-08 22:09:46 -0400

    wip: build installer

--- src/compile/__main__.py
+++ src/compile/__main__.py
@@ -3,25 +3,50 @@ import os
 from utils.process import run
 
 
-def build_shim(name: str):
-    src_base = "src"
-    src_file = os.path.realpath(os.path.join(__file__, "../template.py"))
+def _get_template_file_path(filename: str) -> str:
+    return os.path.realpath(os.path.join(__file__, f"../templates/{filename}.py"))
+
+
+def _build_dist(name: str):
+    dist_path = "dist"
     run(
         "pyinstaller",
         "-F",
         "-n",
         name,
         "-p",
-        src_base,
+        "src",
+        "--distpath",
+        dist_path,
         "--specpath",
         "artifacts",
-        src_file,
+        _get_template_file_path(name),
     )
+    return os.path.join(dist_path, name)
+
+
+def build_installer(shim_bin: bytes):
+    """Build installer exec for a particular nvshim version binary"""
+    name = "installer"
+
+    py_file_path = _get_template_file_path(name)
+    with open(py_file_path, "r") as f:
+        content = f.read().replace("{SHIM_BIN_PLACEHOLDER}", str(shim_bin))
+    with open(py_file_path, "w") as f:
+        f.write(content)
+
+    return _build_dist(name)
+
+
+def build_shim() -> str:
+    """Build shim executable and return path to distributable"""
+    return _build_dist("shim")
 
 
 def main():
-    for shim in ["node", "npm", "npx"]:
-        build_shim(shim)
+    shim_path = build_shim()
+    with open(shim_path, "rb") as shim_file:
+        build_installer(shim_file.read())
 
 
 if __name__ == "__main__":
---
+++ src/compile/templates/installer.py
@@ -0,0 +1,11 @@
+from utils.process import run
+
+
+def main():
+    shim_hex = {SHIM_BIN_PLACEHOLDER}
+    shim_names = ["node", "npm", "npx"]
+    print("TODO: implement installer")
+
+
+if __name__ == "__main__":
+    main()
