ba3c20868e6722a76f348437c607b059c55ab468
Emmanuel Ogbizi <emmanuel.ogbizi-ugbe@tophatmonocle.com>
2019-09-10 11:03:49 -0400

    wip: generate test and install requires

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+import os
 from setuptools import setup
+from typing import Sequence
+
 from compiler import __version__
 
 
@@ -7,48 +10,63 @@ def readme() -> str:
         return f.read()
 
 
-def get_requirements(filepath: str, parents: [str]) -> [str]:
-    requirements = set()
+def lines(filepath) -> Sequence[str]:
+    """Lines of a file generator"""
     with open(filepath) as f:
-        parents.append(filepath)
         while True:
             line = f.readline().strip()
-            req_file_delim = "-r"
-            req_package_delim = "=="
-            if not line:
-                break
-            elif line.startswith(req_file_delim):
-                nested_requirements_file = line.split(req_file_delim)[1].trim()
-                if nested_requirements_file in parents:
-                    print("Recursive requirement dependency chain", filepath, parents)
-                    break
+            if line:
+                yield line
+            else:
+                return
 
-                requirements.union(get_requirments(nested_requirements_file, parents))
+
+def get_requirements(filepath: str, visited: [str]) -> [str]:
+    """
+    Get all pip requirements specified by a requirements file
+    with support for nested requirements files
+
+    :param filepath: path to requirements.txt file
+    :param visited: mutable list of visited requirements.txt files
+    :return: unordered list of requirements without versions
+    """
+    requirements = set()
+    filepath = os.path.realpath(filepath)
+    rel_filepath = os.path.relpath(filepath)
+
+    if filepath in visited:
+        print("Skipping requirements:", rel_filepath)
+        print(visited)
+
+    else:
+        print("Parsing requirements:", rel_filepath)
+        visited.append(filepath)
+        req_file_delim = "-r"
+        req_package_delim = "=="
+        requirements_dir = os.path.dirname(filepath)
+        for line in lines(filepath):
+            if line.startswith(req_file_delim):
+                nested_req_file = line.split(req_file_delim)[1].strip()
+                nested_req_filepath = os.path.join(requirements_dir, nested_req_file)
+                requirements.union(get_requirements(nested_req_filepath, visited))
+
             elif req_package_delim in line:
                 requirements.add(line.split(req_package_delim)[0])
 
     return list(requirements)
 
 
-def install_requirements() -> [str]:
-    return get_requirements("requirements/prod.txt", [])
-
-
-def test_requirements() -> [str]:
-    return get_requirements("requirements/test.txt", [])
-
-
 setup(
     author="Emmanuel Ogbizi-Ugbe",
     author_email="iamogbz+pypi@gmail.com",
     description="Automagically use the correct version of node",
-    install_requires=install_requirements(),
+    install_requires=get_requirements("requirements/prod.txt", []),
     keywords="node nvm node-shim shim shell nvm-shim",
     long_description=readme(),
     license="GNU",
     name="nvshim",
     scripts=[f"dist/{filename}" for filename in ["node", "npm", "npx"]],
-    tests_require=test_requirements(),
+    tests_require=get_requirements("requirements/test.txt", []),
     url="http://github.com/iamogbz/nvshim",
     version=__version__,
 )
