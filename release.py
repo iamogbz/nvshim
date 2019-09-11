b7d407e9c3f8e0748ba3f9b1afe24785a589c96e
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2019-09-11 01:30:46 -0400

    wip: remove release script

--- release.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from semantic_release import cli
-
-try:
-    ARGS = sorted(sys.argv[1:], key=lambda x: 1 if x.startswith("--") else -1)
-    cli.main(args=ARGS)
-except Exception as e:
-    print(e)
