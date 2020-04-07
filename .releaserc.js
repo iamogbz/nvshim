4a0df30c4c0efb56a4c19b5119c1df9795a0b339
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-04-07 17:10:30 -0400

    chore: point to correct user repo

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,4 @@
+const user_repo = "iamogbz/nvshim"
 module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
@@ -11,13 +12,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## [In Progress](https://github.com/tophat/syrupy/pulls)
+## [In Progress](https://github.com/${user_repo}/pulls)
 
-See open [issues](https://github.com/tophat/syrupy/issues)
+See open [issues](https://github.com/${user_repo}/issues)
 
-## [Unreleased](https://github.com/tophat/syrupy/releases)
+## [Unreleased](https://github.com/${user_repo}/releases)
 
-See closed [issues](https://github.com/tophat/syrupy/pulls?q=is%3Apr+is%3Amerged+sort%3Acreated-desc+-label%3Areleased)`,
+See closed [issues](https://github.com/${user_repo}/pulls?q=is%3Apr+is%3Amerged+sort%3Acreated-desc+-label%3Areleased)`,
             },
         ],
         ["@semantic-release/git", { assets: ["CHANGELOG.md"] }],
